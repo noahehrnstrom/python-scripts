447bb7059add11e5b6d5063020ae06c756f3bc79
noahehrnstrom <44081316+noahehrnstrom@users.noreply.github.com>
2018-10-13 18:31:31 +0200

    Add files via upload

---
+++ Google.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from time import sleep
+from random import randint
+tk = Tk()
+c = Canvas(tk, width=1000, height=800)
+tk.wm_attributes("-topmost", 1)
+c.pack()
+a = 1
+while True:
+ if randint(1,4) == 1:
+  c.create_text(500, 800, text="Google", font=("Courier", 200), fill="red")
+ elif randint(1,4) == 2:
+  c.create_text(500, 800, text="Google", font=("Courier", 200), fill="blue")
+ elif randint(1,4) == 3:
+  c.create_text(500, 800, text="Google", font=("Courier", 200), fill="orange")
+ else:
+  c.create_text(500, 800, text="Google", font=("Courier", 200), fill="green")
+ for i in range(100):
+  c.move(a, 0, -9.6)
+  sleep(0.01)
+  tk.update()
+ a = a + 1
