45bc12e08d594a1a2a69e6fcdc94b4fc00aa358a
noahehrnstrom <44081316+noahehrnstrom@users.noreply.github.com>
2018-10-21 10:51:54 +0200

    Google.py
    the random function in the if-statement  wasn't complete

--- Google.py
+++ Google.py
@@ -1,17 +1,23 @@
 from tkinter import *
 from time import sleep
 from random import randint
+
 tk = Tk()
 c = Canvas(tk, width=1000, height=800)
 tk.wm_attributes("-topmost", 1)
 c.pack()
+
 a = 1
+
 while True:
- if randint(1,4) == 1:
+ 
+ rand = randint(1, 4)
+ 
+ if rand == 1:
   c.create_text(500, 800, text="Google", font=("Courier", 200), fill="red")
- elif randint(1,4) == 2:
+ elif rand == 2:
   c.create_text(500, 800, text="Google", font=("Courier", 200), fill="blue")
- elif randint(1,4) == 3:
+ elif rand == 3:
   c.create_text(500, 800, text="Google", font=("Courier", 200), fill="orange")
  else:
   c.create_text(500, 800, text="Google", font=("Courier", 200), fill="green")
